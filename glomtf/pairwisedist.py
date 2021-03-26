f8c770afd158136f9e2eb6fe692d8f52c51a52b1
Rishit Dagli <rishit.dagli@gmail.com>
2021-03-26 21:25:27 +0530

    Add supplementary comments about 3 major operations

--- glomtf/pairwisedist.py
+++ glomtf/pairwisedist.py
@@ -2,11 +2,14 @@ import tensorflow as tf
 
 def pairwise_dist(A, B):
 
+    # squared norms of each row in A and B
     na = tf.reduce_sum(tf.square(A), 1)
     nb = tf.reduce_sum(tf.square(B), 1)
 
+    # na as a row and nb as a column vectors
     na = tf.reshape(na, [-1, 1])
     nb = tf.reshape(nb, [1, -1])
 
+    # return pairwise euclidean difference matrix
     D = tf.sqrt(tf.maximum(na - 2 * tf.matmul(A, B, False, True) + nb, 0.0))
     return D
