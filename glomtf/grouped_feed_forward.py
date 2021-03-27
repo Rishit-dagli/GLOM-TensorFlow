be439686dd769e86fa17342cebdade17cf7ea321
Rishit Dagli <rishit.dagli@gmail.com>
2021-03-27 11:59:56 +0530

    Fix shape errors for grouped_feed_forward

--- glomtf/grouped_feed_forward.py
+++ glomtf/grouped_feed_forward.py
@@ -6,23 +6,17 @@ class GroupedFeedForward(tf.keras.layers.Layer):
     def __init__(self, dim, groups, mult=4):
         super(GroupedFeedForward, self).__init__()
         total_dim = dim * groups
-        self.net = tf.keras.Sequential(
-            [
-                Rearrange('b n l d -> b (l d) n'),
-                tf.keras.layers.Conv1D(
-                    total_dim,
-                    total_dim * mult,
-                    1,
-                    groups=groups,
-                    activation='gelu'),
-                tf.keras.layers.Conv1D(
-                    total_dim,
-                    total_dim * mult,
-                    1,
-                    groups=groups),
-                Rearrange(
-                    'b (l d) n -> b n l d',
-                    l=groups)])
+        self.net = tf.keras.Sequential([Rearrange('b n l d -> b (l d) n'),
+                                        tf.keras.layers.Conv1D(total_dim,
+                                                               total_dim * mult,
+                                                               1,
+                                                               groups=groups,
+                                                               activation='gelu'),
+                                        tf.keras.layers.Conv1D(total_dim,
+                                                               total_dim * mult,
+                                                               1,
+                                                               groups=groups),
+                                        Rearrange('b (l d) n -> b n l d', l=groups)])
 
-    def call(self, inputs):
-        return self.net(inputs)
+        def call(self, inputs):
+            return self.net(inputs)
